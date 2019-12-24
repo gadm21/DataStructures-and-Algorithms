261179eb0216494c10106a520a2cce0cbb3742ce
gad <g.gad@nu.edu.eg>
2019-12-25 00:40:30 +0200

    started math section with incompleted Prob

---
+++ Algorithms/math/death_in_circle.cpp
@@ -0,0 +1,13 @@
+//URL:: https://practice.geeksforgeeks.org/problems/game-of-death-in-a-circle/0
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n, k; cin>>n>>k;
+
+    }
+}
