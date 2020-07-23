7a1876c4d512a72edd0a0cbbfe397389504b7488
unknown <g.gad@nu.edu.eg>
2020-07-23 17:18:28 +0200

    first eyad commit

---
+++ DataStructures/LinkedList/eyad_SingleLinkedList.py
@@ -0,0 +1,32 @@
+
+
+
+class SingleLinkedList(object):
+
+    def __init__(self):
+        #define member variables    
+        pass 
+
+    def insert(self, element, position = None):
+        pass 
+
+    def delete(self, element=None, position=None):
+        if not element and not position : return False
+    
+    def merge(self, AnotherList, position = None):
+        pass
+
+    def find(self, element):
+        #return true if element found
+        pass 
+
+    def search(self, element):
+        #return position if element found or -1 if not found
+        pass 
+
+#add [] feature
+#add printability feature 
+#add + feature aginst other lists 
+
+    def sort(self):
+        pass 
